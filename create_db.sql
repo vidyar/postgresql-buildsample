f136bd317fc22398ee27bc41a7ec8c85d8a44c36
vamshis <vamshis@qhode.com>
2013-11-07 11:11:18 +0530

    Update create_db.sql

--- create_db.sql
+++ create_db.sql
@@ -1,7 +1,7 @@
-drop database if exists foodb;
+drop database if exists E801AAEC8BA9F6066_foodb;
 
-create database foodb;
+create database E801AAEC8BA9F6066_foodb;
 
-\c foodb;
+\c E801AAEC8BA9F6066_foodb;
 
 create table T1(foo int);
