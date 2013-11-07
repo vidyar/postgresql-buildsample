2b2754948fbfdb8b935d29115794c5dd2219e14b
vamshis <vamshis@qhode.com>
2013-11-07 11:11:47 +0530

    Update pgsqlsample.py

--- pgsqlsample.py
+++ pgsqlsample.py
@@ -1,6 +1,6 @@
 import psycopg2
 
-conn = psycopg2.connect("dbname='foodb' user='E801AAEC8BA9F6066' password='EyiMQNPNVfQG9!'")
+conn = psycopg2.connect("dbname='E801AAEC8BA9F6066_foodb' user='E801AAEC8BA9F6066' password='EyiMQNPNVfQG9!'")
 cur = conn.cursor();
 
 cur.execute("""insert into T1 (foo) values (10) """)
