eb90021c4b28cceb14f5330f6f9c3fdc72e53c45
Vamshi Suram <vamshis@qhode.com>
2013-11-08 12:05:31 +0530

    creating database from shippable.yml

--- pgsqlsample.py
+++ pgsqlsample.py
@@ -3,9 +3,12 @@ import psycopg2
 conn = psycopg2.connect("dbname='foodb' user='shippable'")
 cur = conn.cursor();
 
+cur.execute("""create table T1 (foo int)""")
+
 cur.execute("""insert into T1 (foo) values (10) """)
 
 cur.execute("""select * from T1""")
+
 rows = cur.fetchall()
 for row in rows:
   print row[0]
